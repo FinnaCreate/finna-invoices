f7c967b0d378b2c36f9bd8584c0439f9dbb4aa8a
FinnaCreate <finnacreate@gmail.com>
2022-02-27 17:37:09 +0000

    Modular admin submenu's/pages

--- inc/Pages/Admin.php
+++ inc/Pages/Admin.php
@@ -9,27 +9,60 @@ namespace Inc\Pages;
 use \Inc\Base\BaseController;
 use \Inc\Api\SettingsApi;
 
-/**
-*
-*/
 class Admin extends BaseController
 {
-    // public $settings;
+    public $settings;
 
-    // public function __construct()
-    // {
-    //     $this->settings = new SettingsApi();
-    // }
+    public $pages = [];
 
-	public function register() {
-		add_action( 'admin_menu', array( $this, 'add_admin_pages' ) );
-	}
+    public $subpages = [];
 
-	public function add_admin_pages() {
-        add_menu_page($this->plugin_name, $this->plugin_name, 'manage_options', $this->plugin_slug, array( $this, 'admin_index' ), 'dashicons-book-alt', 110 );
-	}
+    public function __construct()
+    {
+        $this->settings = new SettingsApi();
 
-	public function admin_index() {
-		require_once $this->plugin_path . '/views/admin.php';
+        $this->pages = [
+            [
+                'page_title' => 'Finna Invoices',
+                'menu_title' => 'Finna Invoices',
+                'capability' => 'manage_options',
+                'menu_slug' => 'finna_invoices',
+                'callback' => function() { echo '<h1>Finna Invoices</h1>';},
+                'icon_url' => 'dashicons-book-alt',
+                'position' => 110
+            ]
+        ];
+
+        $this->subpages = [
+			[
+				'parent_slug' => 'finna_invoices',
+				'page_title' => 'Custom Post Types',
+				'menu_title' => 'CPT',
+				'capability' => 'manage_options',
+				'menu_slug' => 'finna_cpt',
+				'callback' => function() { echo '<h1>CPT Manager</h1>'; }
+			],
+			[
+				'parent_slug' => 'finna_invoices',
+				'page_title' => 'Custom Taxonomies',
+				'menu_title' => 'Taxonomies',
+				'capability' => 'manage_options',
+				'menu_slug' => 'finna_taxonomies',
+				'callback' => function() { echo '<h1>Taxonomies Manager</h1>'; }
+			],
+			[
+				'parent_slug' => 'finna_invoices',
+				'page_title' => 'Custom Widgets',
+				'menu_title' => 'Widgets',
+				'capability' => 'manage_options',
+				'menu_slug' => 'finna_widgets',
+				'callback' => function() { echo '<h1>Widgets Manager</h1>'; }
+			]
+		];
+    }
+
+	public function register()
+    {
+        $this->settings->addPages($this->pages)->withSubPage('Dashboard')->addSubPages($this->subpages)->register();
 	}
 }
--- views/admin.php
+++ views/admin.php
@@ -1,1 +1,1 @@
-<h1 class="mt-4 mr-2 text-2xl font-semibold">Finna Invoices</h1>
+<h1>Finna Invoices</h1>
